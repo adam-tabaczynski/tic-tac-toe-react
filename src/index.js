9cf1785686df2cdab09df7d41365ec6d5f75dfc9
Adam Tabaczynski <adam.tabaczynski96@gmail.com>
2020-08-09 18:46:28 +0200

    Lift state up and add handleClick method
    State has been lifted up to Board component.
    Added handleClick method to be able to put X's or O's in Squares
    from Board's component.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-constructor(props) {
-    super(props);
-    this.state = {
-        value: null,
-        };
-}
   render() {
     return (
       <button 
         className="square" 
-        onClick={() => this.setState({value: 'X'}) }
-        >
-        {this.state.value}
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
-
+// State has been moved from Square to Board for further
+// advancement of the game - it is needed to determine the winner.
+// The Squares are still clickable, yet they rerender accordingly
+// to changes of Board's state.
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  // the .slice() allows creating a copy of squares array
+  // instead of modifying OG array. This allows immutability
+  // of objects, which in exchange gives some benefits:
+  // 1) Avoiding direct data change allows to simplify complex
+  // functionalities like game history,
+  // 2) Detecting changes in objects is much easier;
+  // 3) Not changing objects allow creating pure components in
+  // React and easify determining when component requires 
+  // re-rendering.
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
