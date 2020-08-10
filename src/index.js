19a355d1dbe6a4ea3f2d7beb22551d9cb716f2f7
Adam Tabaczynski <adam.tabaczynski96@gmail.com>
2020-08-10 21:32:53 +0200

    Lift state up to Game and fix bug in calculateWin
    State has been moved from Board to Game component for further
    improvements - showing game history.
    Now State contains array of Board configurations (history)
    which will be accessed later on to show which moves happened.
    There was misplaced ending curly bracket in calculateWinner
    method which were causing no game ending in winning configurations.

--- src/index.js
+++ src/index.js
@@ -9,72 +9,88 @@ function Square(props) {
     </button>
   )
 }
-// State has been moved from Square to Board for further
-// advancement of the game - it is needed to determine the winner.
-// The Squares are still clickable, yet they rerender accordingly
-// to changes of Board's state.
+
+// // State has been moved from Square to Board for further
+// // advancement of the game - it is needed to determine the winner.
+// // The Squares are still clickable, yet they rerender accordingly
+// // to changes of Board's state.
+
+// State has been lifted up further to Game component for next
+// game improvements.
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     squares: Array(9).fill(null),
+  //     xIsNext: true,
+  //   };
+  // }
 
-  // the .slice() allows creating a copy of squares array
-  // instead of modifying OG array. This allows immutability
-  // of objects, which in exchange gives some benefits:
-  // 1) Avoiding direct data change allows to simplify complex
-  // functionalities like game history,
-  // 2) Detecting changes in objects is much easier;
-  // 3) Not changing objects allow creating pure components in
-  // React and easify determining when component requires 
-  // re-rendering.
-  // Here, square gets the value depending on which player in 
-  // his turn clicked the square.
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    // This if statement does two things - firstly, if Square
-    // already has X or O inside it, player's clicking the field will
-    // not change the square's value (and will not change the turn).
-    // secondly, after winning the game by either of players, clicking
-    // the squares will not change their value.
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
+  // // the .slice() allows creating a copy of squares array
+  // // instead of modifying OG array. This allows immutability
+  // // of objects, which in exchange gives some benefits:
+  // // 1) Avoiding direct data change allows to simplify complex
+  // // functionalities like game history,
+  // // 2) Detecting changes in objects is much easier;
+  // // 3) Not changing objects allow creating pure components in
+  // // React and easify determining when component requires 
+  // // re-rendering.
+  // // Here, square gets the value depending on which player in 
+  // // his turn clicked the square.
+  // handleClick(i) {
+  //   const squares = this.state.squares.slice();
 
+  //   // This if statement does two things - firstly, if Square
+  //   // already has X or O inside it, player's clicking the field will
+  //   // not change the square's value (and will not change the turn).
+  //   // secondly, after winning the game by either of players, clicking
+  //   // the squares will not change their value.
+  //   if (calculateWinner(squares) || squares[i]) {
+  //     return;
+  //   }
+  //   squares[i] = this.state.xIsNext ? 'X' : 'O';
+  //   this.setState({
+  //     squares: squares,
+  //     xIsNext: !this.state.xIsNext,
+  //   });
+  // }
+
+  // whole handleClick(i) method has been moved upward to Game component
+  // and has been changed.
+
+  // here we change state to props due to lifting state up
+  // to Game component.
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
-  // Here has been added mechanism to decide the winner base on
-  // calucation from calculateWinner() method. In case nobody wins
-  // (yet), the 'Next player' text will change accordingly to player's
-  // turn.
+
+  // // Here has been added mechanism to decide the winner base on
+  // // calucation from calculateWinner() method. In case nobody wins
+  // // (yet), the 'Next player' text will change accordingly to player's
+  // // turn.
+
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    }
-    else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+    // const winner = calculateWinner(this.state.squares);
+    // let status;
+    // if (winner) {
+    //   status = 'Winner: ' + winner;
+    // }
+    // else {
+    //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // }
+
+    // Above text information has been moved to Game component.
+    // Below, one line
+    // <div className="status">{status}</div>
+    // has been removed due to it's obsoleteness
     
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -95,15 +111,78 @@ class Board extends React.Component {
   }
 }
 
+// We want to introduce 'game history' option,
+// to that we will need lift the state up yet again,
+// previously from Square to Board, now from Board to Game.
+// It will allow to keep the 'history' and keep full control
+// over Board's data and let us render previous moves based on
+// the 'history'.
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true
+    };
+  }
+
+  // Information about how handleClick works is in Board component.
+  // Its due to the fact that initially that function was there, before
+  // lifitng state up to Game comp.
+  handleClick(i) {
+
+    // history contains array of Board configurations after each turn.
+    // Then we get into current configuration on board assigned to 
+    // current constant.
+    // After that, winner calcualting, etc. works similiar as earlier.
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    // Major change occurs here - initially, the board in empty.
+    // After clicking any Square, the handleClick(i) method activates,
+    // Current Board config is copied, the winner condition is checked,
+    // Then the square which was clicked will be assigned the value of
+    // X or O. After that, the new state of Board is concatenated to
+    // game history (the .push() instead of .concat() would mutate the
+    // array) and with that we have access to Board configuration after
+    // each step.
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    }
+    else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+            />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
@@ -131,11 +210,11 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let i =0; i < lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
-    return null;
   }
+    return null;
 }
